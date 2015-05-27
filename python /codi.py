2db436750664b9df0d6d60f09a2beb2691225fed
Garreta11 <jordigarreta11@gmail.com>
2015-05-27 15:10:54 +0200

    New code.py file

--- python /codi.py
+++ python /codi.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import bs4
 
@@ -5,33 +6,34 @@ i = 0
 
 # comentario de código para mejorar la interacción...
 
-chords = ('C','C#','Cm',
-'Db','D','D#','Dm',
-'Eb','E','Em',
-'F','F#','Fm',
-'Gb','G','G#','Gm',
-'Ab','A','A#','Am','Am7',
-'Bb','B','Bm')
+#chords = ('C','C#','Cm',
+#'Db','D','D#','Dm',
+#'Eb','E','Em',
+#'F','F#','Fm',
+#'Gb','G','G#','Gm',
+#'Ab','A','A#','Am','Am7',
+#'Bb','B','Bm')
 
 
 chords_root = (
-'C','C#','Cb',
-'D','D#','Db',
-'E','E#','Eb',
-'F','F#','Fb',
-'G','G#','Gb',
-'A','A#','Ab',
-'B','B#','Bb')
+'Cb','C','C#',
+'Db','D','D#',
+'Eb','E','E#',
+'Fb','F','F#',
+'Gb','G','G#',
+'Ab','A','A#',
+'Bb','B','B#')
 
 chords_type = ('', 'm', 'M','aug','dim','7')
-'''
-si aparece la combinacion de los dos
-'''
-'''
+
+chords = []
+
 for chords1 in chords_root:
     for chords2 in chords_type:
-        chords = chords1+chords2
-'''
+        final_chords = chords1+chords2
+        chords.append(final_chords)
+
+chords = tuple(chords)
 
 url = requests.session()
 top100 = url.get('http://www.ultimate-guitar.com/top/top100.htm');
@@ -52,26 +54,53 @@ for link in codi.find_all('a'):
             f = open(text_file_name,'w')
             endurl = len(songs)-4
             song_name = songs[34:endurl]
-            f.write(song_name + '\n')
+            #f.write(song_name + '\n')
             for link in doc.find_all('span'):
                 for child in link.children:
-                    '''
-                    ignorar lo que aparece a la derecha del slash (incuido)
-                    '''
                     if child in chords:
-                        '''
+                        #print child
+                                                
+                        #Enharmonic Chords
+                        child = child.replace('C#','Db')
+                        child = child.replace('D#','Eb')
+                        child = child.replace('F#','Gb')
+                        child = child.replace('G#','Ab')
+                        child = child.replace('A#','Bb')
+                                                
+                        chords_root = (
+                        'Cb','C','C#',
+                        'Db','D','D#',
+                        'Eb','E','E#',
+                        'Fb','F','F#',
+                        'Gb','G','G#',
+                        'Ab','A','A#',
+                        'Bb','B','B#')
+                        
+                        chords_type = ('m', 'M','aug','dim','7')                  
+                        
+                        #if child[1] == 'b':
+                        #    child = child[:2]+':'+child[2:]
+                        #else:
+                        #    child = child[:1]+':'+child[1:]
+                        
+                        if child.startswith('b',1):
+                            if not child.startswith('',2):
+                                child = child[:2]+':'+child[2:]
+                        elif child.startswith('m',1):
+                            child = child[:1]+':'+child[1:]
+                        elif child.startswith('M',1):
+                            child = child[:1]+':'+child[1:]
+                        elif child.startswith('aug',1):
+                            child = child[:1]+':'+child[1:]
+                        elif child.startswith('dim',1):
+                            child = child[:1]+':'+child[1:]
+                        elif child.startswith('7',1):
+                            child = child[:1]+':'+child[1:]
+                            
+                            
+                        child = str(child)
                         print child
-                        '''
+                            
                         f = open(text_file_name,'a')
                         f.write(child + '\n')
                         f.close
-'''
-- El acorde inicial     ------------DONE----------------
-- El acorde final       -----------NOT DONE-------------
-- Quitar duplicados (ordenado!)
-- Histograma de los acordes --> grados 1, 4 y 5 son los mas importantes
-- Estimar la tonalidad con los datos anteriores
-- Encontrar patrones (de 3 a 8 acordes) dentro de la misma cancion (min 3 veces)
-- histograma total de todas las canciones (como estadistica) --------DONE-------
-
-'''
