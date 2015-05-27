d2c0297465e21f077acd72414a8264fd33e0e1b7
Garreta11 <jordigarreta11@gmail.com>
2015-05-27 19:46:24 +0200

    Final python codi.py

--- python /codi.py
+++ python /codi.py
@@ -58,49 +58,62 @@ for link in codi.find_all('a'):
             for link in doc.find_all('span'):
                 for child in link.children:
                     if child in chords:
-                        #print child
-                                                
-                        #Enharmonic Chords
+                                                                        
+                        #----Enharmonic Chords----
                         child = child.replace('C#','Db')
                         child = child.replace('D#','Eb')
                         child = child.replace('F#','Gb')
                         child = child.replace('G#','Ab')
                         child = child.replace('A#','Bb')
-                                                
-                        chords_root = (
-                        'Cb','C','C#',
-                        'Db','D','D#',
-                        'Eb','E','E#',
-                        'Fb','F','F#',
-                        'Gb','G','G#',
-                        'Ab','A','A#',
-                        'Bb','B','B#')
-                        
-                        chords_type = ('m', 'M','aug','dim','7')                  
-                        
-                        #if child[1] == 'b':
-                        #    child = child[:2]+':'+child[2:]
-                        #else:
-                        #    child = child[:1]+':'+child[1:]
-                        
-                        if child.startswith('b',1):
-                            if not child.startswith('',2):
-                                child = child[:2]+':'+child[2:]
+                       
+                        #----Good Format of Representation----
+                        if child.endswith('b'):
+                            child = child[:2]+':'+child[2:]
                         elif child.startswith('m',1):
                             child = child[:1]+':'+child[1:]
                         elif child.startswith('M',1):
                             child = child[:1]+':'+child[1:]
+                            #print child ----NO FA RES!!!
                         elif child.startswith('aug',1):
                             child = child[:1]+':'+child[1:]
                         elif child.startswith('dim',1):
                             child = child[:1]+':'+child[1:]
                         elif child.startswith('7',1):
                             child = child[:1]+':'+child[1:]
-                            
-                            
+                        elif child.startswith('sus4',1):
+                            child = child[:1]+':'+child[1:]
+                         
+                        #----m = min----
+                        if child.endswith('dim'):
+                            child = child.replace('dim',':dim')
+                        elif child.endswith('m'):
+                            child = child.replace('m','min')
+                        
+                        if child.endswith(':'):
+                            child = child.replace(':',':maj')
+                                                
+                        if child.endswith('C'):
+                            child = child.replace('C','C:maj') 
+                        if child.endswith('D'):
+                            child = child.replace('D','D:maj') 
+                        if child.endswith('E'):
+                            child = child.replace('E','E:maj') 
+                        if child.endswith('F'):
+                            child = child.replace('F','F:maj') 
+                        if child.endswith('G'):
+                            child = child.replace('G','G:maj') 
+                        if child.endswith('A'):
+                            child = child.replace('A','A:maj') 
+                        if child.endswith('B'):
+                            child = child.replace('B','B:maj') 
+                        
+                        if child.endswith('bmin'):
+                            child = child.replace('bmin','b:min')
+                                                
                         child = str(child)
                         print child
-                            
+                        
+                        #----Write to each file----
                         f = open(text_file_name,'a')
                         f.write(child + '\n')
                         f.close
